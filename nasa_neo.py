db089dca4f159a33d5d7f3dbbe1ca1d8637409c7
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2018-03-08 23:52:42 +0000

    Add files via upload
    This version asks for a date, displays how many NEOs existed on that date, data about each NEO and if the NEO was potentially hazadous.

--- nasa_neo.py
+++ nasa_neo.py
@@ -3,31 +3,56 @@
 #NASA NEO
 #Big thanks to NASA API (https://api.nasa.gov/api.html), get your API key from this site.
 
-import json, urllib.request
+import json, requests
 
 #variables
-api='DEMO_KEY'
+api="DEMO_KEY"
 start_date=""
 end_date=""
 result=""
-potentially_hazardous=""
+n=""
+
 
 #message to welcome the user
 print('Hi and welcome to GeekTechStuff\'s NASA Near Earth Object (NEO) Python program')
 print('This programme uses NASA\'s open API system')
-print('')
+print('This Python program searches a data for NEOs')
+print("")
 
 #asks the users for dates, these must be in yyyy-mm-dd format
 start_date=input('Please enter an eight digit date in yyyy-mm-dd format for the start date:')
-end_date=input('Please enter an eight digit date in yyyy-mm-dd format for the end date:')
+#end_date=input('Please enter an eight digit date in yyyy-mm-dd format for the end date:')
+end_date=start_date
 
 #opens JSON file containing NEO data
 url='https://api.nasa.gov/neo/rest/v1/feed?start_date='+start_date+'&end_date='+end_date+'&api_key='+api
-response=urllib.request.urlopen(url)
-result=json.loads(response.read())
+response=requests.get(url)
+response.raise_for_status()
+neodata=json.loads(response.text)
+number_of_neo=neodata["element_count"]
+nd=neodata["near_earth_objects"]
+nd2=nd[start_date]
 print('Loading data from',url)
 print('')
 
 #uses the json loaded from get_json
-print('There were',result['element_count'],'NEO\'s between',start_date,'and',end_date)
+print('On',start_date,'there were',number_of_neo,'near Earth objects')
+print("")
+neo_range=range(number_of_neo)
+for n in neo_range:
+    print("Neo Ref:")
+    print(nd2[n]['neo_reference_id'])
+    print("Neo Name:")
+    print(nd2[n]['name'])
+    print("Check out the NASA page for the NEO at:")
+    print(nd2[n]['nasa_jpl_url'])
+    if nd2[n]["is_potentially_hazardous_asteroid"] == False:
+        print('It was not potentially hazardous')
+    else:
+        print('It was potentially hazardous')
+    print("")
+  
+
+
+
 
